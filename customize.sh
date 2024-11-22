5b4880a9ce067ebb35d91829f52f96d6554fa58f
coomsn <61256281+coomsn@users.noreply.github.com>
2024-11-22 13:10:58 +0800

    up

--- customize.sh
+++ customize.sh
@@ -1,14 +1,14 @@
 #!/system/bin/sh
 ##########################
-# xray_lite Customization#
+# xray_tproxy Customization#
 ##########################
 SKIPUNZIP=1
 ASH_STANDALONE=1
 unzip_path="/data/adb"
 
 # Define the paths of source folder and the destination folder
-source_folder="/data/adb/xray_lite"
-destination_folder="/data/adb/xray_lite$(date +%Y%m%d_%H%M%S)"
+source_folder="/data/adb/xray_tproxy"
+destination_folder="/data/adb/xray_tproxy$(date +%Y%m%d_%H%M%S)"
 
 # Check if the source folder exists
 if [ -d "$source_folder" ]; then
@@ -23,35 +23,35 @@ else
 fi
 
 ui_print "- 正在释放文件"
-unzip -o "$ZIPFILE" 'xray_lite/*' -d $unzip_path >&2
-unzip -j -o "$ZIPFILE" 'xray_lite_service.sh' -d /data/adb/service.d >&2
+unzip -o "$ZIPFILE" 'xray_tproxy/*' -d $unzip_path >&2
+unzip -j -o "$ZIPFILE" 'xray_tproxy_service.sh' -d /data/adb/service.d >&2
 unzip -j -o "$ZIPFILE" 'uninstall.sh' -d $MODPATH >&2
 unzip -j -o "$ZIPFILE" "module.prop" -d $MODPATH >&2
 ui_print "- 正在设置权限"
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm_recursive /data/adb/xray_lite/ 0 3005 0755 0644
-set_perm_recursive /data/adb/xray_lite/scripts/ 0 3005 0755 0700
-set_perm /data/adb/service.d/xray_lite_service.sh 0 0 0755
+set_perm_recursive /data/adb/xray_tproxy/ 0 3005 0755 0644
+set_perm_recursive /data/adb/xray_tproxy/scripts/ 0 3005 0755 0700
+set_perm /data/adb/service.d/xray_tproxy_service.sh 0 0 0755
 set_perm $MODPATH/uninstall.sh 0 0 0755
-set_perm /data/adb/xray_lite/scripts/ 0 0 0755
+set_perm /data/adb/xray_tproxy/scripts/ 0 0 0755
 ui_print "- 完成权限设置"
 ui_print "- 还原配置文件"
 
 # 找到文件夹对应的最大的数字
-largest_folder=$(find /data/adb -maxdepth 1 -type d -name 'xray_lite[0-9]*' | sed 's/.*xray_lite//' | sed 's/_//g' | sort -nr | head -n 1)
+largest_folder=$(find /data/adb -maxdepth 1 -type d -name 'xray_tproxy[0-9]*' | sed 's/.*xray_tproxy//' | sed 's/_//g' | sort -nr | head -n 1)
 
 # 使用这个最大的数字，重新匹配回原始文件夹名
 if [ -n "$largest_folder" ]; then
-  for folder in /data/adb/xray_lite*; do
-    clean_name=$(echo "$folder" | sed 's/.*xray_lite//' | sed 's/_//g')
+  for folder in /data/adb/xray_tproxy*; do
+    clean_name=$(echo "$folder" | sed 's/.*xray_tproxy//' | sed 's/_//g')
     if [ "$clean_name" = "$largest_folder" ]; then
       ui_print "- Found folder: $folder"
       
-      # 覆盖 /data/adb/xray_lite/confs 目录中的内容
+      # 覆盖 /data/adb/xray_tproxy/confs 目录中的内容
       if [ -d "$folder/confs" ]; then
-        cp -rf "$folder/confs/"* /data/adb/xray_lite/confs/
-        cp -f "$folder/scripts/settings.ini" /data/adb/xray_lite/scripts/
-        ui_print "- Copied contents of $folder/confs to /data/adb/xray_lite/confs/"
+        cp -rf "$folder/confs/"* /data/adb/xray_tproxy/confs/
+        cp -f "$folder/scripts/settings.ini" /data/adb/xray_tproxy/scripts/
+        ui_print "- Copied contents of $folder/confs to /data/adb/xray_tproxy/confs/"
         ui_print "- 成功还原配置文件"
         ui_print "- 成功还原settings.ini"
       fi
