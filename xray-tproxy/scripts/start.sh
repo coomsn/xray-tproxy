0e3fba05ae890c9640330780b7f7602ac9661293
coomsn <61256281+coomsn@users.noreply.github.com>
2024-11-30 13:26:52 +0800

    update

--- xray-tproxy/scripts/start.sh
+++ xray-tproxy/scripts/start.sh
@@ -3,9 +3,9 @@ clear
 scripts=$(realpath $0)
 scripts_dir=$(dirname ${scripts})
 parent_dir=$(dirname ${scripts_dir})
-module_dir="/data/adb/modules/xray_tproxy-module"
+module_dir="/data/adb/modules/xray-tproxy"
 
-source ${scripts_dir}/xray_tproxy.service
+source ${scripts_dir}/xray-tproxy.service
 log Info "The process is starting, please wait"
 
 # Check if the disable file does not exist, then run the proxy
@@ -15,16 +15,15 @@ if [ ! -f "${module_dir}/disable" ]; then
   log Warn "module is not enabled"
 fi
 
-start_xray_tproxy.inotify() {
+start_inotifyd() {
   PIDs=($(busybox pidof inotifyd))
   for PID in "${PIDs[@]}"; do
-    if grep -q "xray_tproxy.inotify" "/proc/$PID/cmdline"; then
+    if grep -q "xray-tproxy.inotify" "/proc/$PID/cmdline"; then
       kill -9 "$PID"
     fi
   done
-  inotifyd "${scripts_dir}/xray_tproxy.inotify" "${module_dir}" >/dev/null 2>&1 &
+  inotifyd "${scripts_dir}/xray-tproxy.inotify" "${module_dir}" >/dev/null 2>&1 &
 }
 
-# Create inotifyd.
-start_xray_tproxy.inotify
-# version2.0
+start_inotifyd
+# {version2.1}
