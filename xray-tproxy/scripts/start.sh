c57e60a0ff89ad8f8c295dc3ab4b7ce94c156639
coomsn <61256281+coomsn@users.noreply.github.com>
2024-12-01 23:01:38 +0800

    Update start.sh

--- xray-tproxy/scripts/start.sh
+++ xray-tproxy/scripts/start.sh
@@ -2,30 +2,16 @@
 clear
 scripts=$(realpath $0)
 scripts_dir=$(dirname ${scripts})
-parent_dir=$(dirname ${scripts_dir})
 module_dir="/data/adb/modules/xray-tproxy"
 # Environment variable settings
 export PATH="/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH"
-
-source ${scripts_dir}/xray-tproxy.service
-if [ ! -f "${module_dir}/disable" ]; then
-  log Info "The process is starting, please wait"
-else
-  log Warn "Please turn on the mask switch"
-fi
-
-# Check if the disable file does not exist, then run the proxy
-if [ ! -f "${module_dir}/disable" ]; then
-  start_tproxy # >/dev/null 2>&1
-  else
-  log Warn "module is not enabled"
-fi
 
 start_inotifyd() {
   PIDs=($(busybox pidof inotifyd))
   for PID in "${PIDs[@]}"; do
     if grep -q "xray-tproxy.inotify" "/proc/$PID/cmdline"; then
       kill -9 "$PID"
+	  break
     fi
   done
   inotifyd "${scripts_dir}/xray-tproxy.inotify" "${module_dir}" >/dev/null 2>&1 &
@@ -33,4 +19,12 @@ start_inotifyd() {
 
 start_inotifyd
 
-# {version2.1}
+# {version:2.1}
+
+source ${scripts_dir}/xray-tproxy.service
+
+if [ ! -f "${module_dir}/disable" ]; then
+  log Info "The process is starting, please wait"
+else
+  log Warn "Please turn on the mask switch"
+fi
